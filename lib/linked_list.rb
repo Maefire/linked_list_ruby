c107688c58929e3595fb4b58e67dc0fe60867430
Maefire <masked_phoenix@yahoo.com>
2023-04-10 00:12:56 -0500

    Implement contains? method
    Now you can search to see if your linked list contains an item or not.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,7 @@ class LinkedList
     @head = nil
   end
 
-  # instantiates a new Node with default nil
+  # instantiates a new Node
   def new_node(value = nil, next_node = nil)
     Node.new(value, next_node)
   end
@@ -34,16 +34,7 @@ class LinkedList
 
   # checks supplied node to see if it is the last in the list
   def last_node?(node)
-    return true if node.next_node.nil?
-
-    false
-  end
-
-  # append(value) adds a new node containing value to the end of the list
-  def append(value)
-    return start_list(value) if head.nil?
-
-    tail.next_node = new_node(value)
+    !node.next_node
   end
 
   # tail returns the last node in the list
@@ -53,24 +44,43 @@ class LinkedList
     tail(node.next_node)
   end
 
+  # append(value) adds a new node containing value to the end of the list
+  def append(value)
+    return start_list(value) if head.nil?
+
+    tail.next_node = new_node(value)
+  end
+
   # at(index) returns the node at the given index
   def at(index, node = head, count = 0)
-    return node if index.eql?(count)
+    return node.value if index.eql?(count)
     return nil if last_node?(node)
 
     at(index, node.next_node, count + 1)
   end
 
   # pop removes the last element from the list
-  def pop
+  def pop(node = head)
+    return "Error: List is empty" if head.nil?
+    return node.next_node = nil if last_node?(node.next_node)
+
+    pop(node.next_node)
   end
 
   # contains?(value) returns true if the passed in value is in the list and otherwise returns false.
-  def contains?(value)
+  def contains?(value, node = head)
+    return true if node.value.eql?(value)
+    return false if last_node?(node)
+
+    contains?(value, node.next_node)
   end
 
   # find(value) returns the index of the node containing value, or nil if not found.
-  def find(value)
+  def find(value, node = head, index = 0)
+    return index if node.value.eql?(value)
+    return nil if last_node?(node)
+
+    find(value, node.next_node, index + 1)
   end
 
   # to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
@@ -83,8 +93,16 @@ class LinkedList
 
   # Extra Credit
   # insert_at(value, index) that inserts a new node with the provided value at the given index.
+  def insert_at(value, index, count = 0)
+    # return start_list(value) if head.nil?
+
+    # return at(index - 1).next_node = new_node(value, at(index)) if index.eql?(count)
+
+    # insert_at(value, index, count + 1)
+  end
 
   # remove_at(index) that removes the node at the given index.
-
+  def remove_at(index)
+  end
   # Extra Credit Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their #next_node link updated.
 end
